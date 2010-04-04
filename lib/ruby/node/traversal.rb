13e30f889377182bff1a4b7a7193068054623863
Kristian Mandrup <kmandrup@gmail.com>
2010-04-04 16:01:38 -0400

    refactored DRY and improved

--- lib/ruby/node/traversal.rb
+++ lib/ruby/node/traversal.rb
@@ -1,6 +1,9 @@
 module Ruby
   class Node
     module Traversal
+      class UnknownArgumentWrapper < StandardError
+      end      
+      
       def select(*args, &block)
         result = []
         result << self if matches?(args.dup, &block)
@@ -37,12 +40,10 @@ module Ruby
           has_namespace?(value)
         when :superclass
           superclass?(value)
-        when :args
+        when :args, :params
           args?(value)                        
-        when :params
-          params?(value)                        
-        when :block_params
-          block_params?(value)                        
+        when :block_params          
+          args?(value, :withblock)          
         when :pos, :position
           position?(value)
         when :right_of
@@ -62,148 +63,136 @@ module Ruby
       end
     end
 
-    # TODO: Needs major refactoring!
-    def args?(value)
-      found = 0
-      if respond_to? :arguments
-        args = arguments.elements
-        args.each do |arg|    
-          argum = arg.arg
-          argument = get_arg(argum) 
-          argument = argument.to_sym if argum.class == Ruby::Symbol
-          argument = argument.to_i if argum.class == Ruby::Integer
-          argument = argument.to_f if argum.class == Ruby::Float
-          value.each do |v|            
-            v = v[:array] if v.respond_to?(:has_key?) && v[:array]              
-            found += 1 if argument == v
-          end
+    def args?(value, with_block = nil)
+      found = 0        
+      obj = with_block ? self.block : self
+      args_list = get_args_list(obj)
+      return false if !args_list    
+      args_list.elements.each do |arg|    
+        argument = retrieve_arg(arg) 
+        value.each do |v|            
+          v = v[:array] if v.respond_to?(:has_key?) && v[:array]              
+          found += 1 if argument == v
         end
-        return found == value.size
-      end
-      false
+      end       
+      return found == value.size
     end                             
 
-    # TODO: Needs major refactoring!
-    def get_arg(arg) 
-      if arg.respond_to? :arg
-        get_arg(arg.arg)
-      elsif arg.respond_to? :token
-        get_token(arg)
-      elsif arg.respond_to? :identifier
-        get_identifier(arg)
-      elsif arg.respond_to? :key
-        get_hash_item(arg)
-      elsif arg.respond_to? :elements
-        e = arg.elements
-        if e.size == 1
-          return get_arg(e[0])
-        end 
+    def get_args_list(obj)                         
+      return obj.params if obj.respond_to? :params 
+      return obj.arguments if obj.respond_to? :arguments 
+      # return obj if obj.class == Ruby::ArgsList       
+    end
 
-        if arg.class == Ruby::Hash 
-          get_hash(e)
-        elsif arg.class == Ruby::Array
-          get_array(e)
-        end           
+    def resolve_arg_wrapper(arg_wrapper)
+      return arg_wrapper.arg if arg_wrapper.respond_to? :arg            
+      return arg_wrapper.param if arg_wrapper.respond_to? :param            
+      return arg_wrapper if arg_wrapper.class == Ruby::Arg
+      arg_wrapper
+    end
+
+    def retrieve_arg(arg_wrapper)
+      arg = resolve_arg_wrapper(arg_wrapper)
+      argument = get_arg(arg)       
+      convert_value(argument, arg)
+    end
+
+    def convert_value(value, type = nil)
+      type_val = type || value
+      case type_val                    
+      when Ruby::Symbol  
+        value.to_sym
+      when Ruby::Integer
+        value.to_i                          
+      when Ruby::Float
+        value.to_f        
+      when Ruby::Hash
+        type ? get_hash(type) : value       
+      when Ruby::Array
+        type ? get_array(type) : value       
+      when Ruby::Assoc   
+        return get_assoc(type)
       else
-        puts "unknown element type"
+        value
       end
+    end      
+
+    def get_arg(arg)  
+      get_symbol(arg) || get_assoc(arg) || get_composite(arg) || get_identifier(arg) || get_token(arg) || resolve_arg_wrapper(arg)
     end
 
-    # TODO: Needs major refactoring!
-    def get_param(arg) 
-      if arg.respond_to? :token
-        get_token(arg)
-      elsif arg.respond_to? :identifier
-        get_identifier(arg)
-      elsif arg.respond_to? :key
-        get_hash_item(arg)
-      elsif arg.respond_to? :elements
-        e = arg.elements
-        if e.size == 1
-          get_param(e[0])
-        end
-        if arg.class == Ruby::Hash 
-          get_hash(e)
-        elsif arg.class == Ruby::Array
-          get_array(e)
-        end           
-      else
-        puts "unknown element type"
-      end        
+    def get_symbol(arg)
+      return nil if !(arg.class == Ruby::Symbol)
+      t = get_token(arg)
+      t ? t.to_sym : nil
     end
 
+    def get_composite(arg)
+      return nil if !arg.respond_to? :elements
+      e = arg.elements
+      return get_arg(e[0]) if e.size == 1
+      get_hash(e) || get_array(e)
+    end
 
     def get_array(args)
+      return nil if arg.class != Ruby::Array
       arr = []  
       args.each do |arg|
          arr << get_arg(arg)
       end
-      arr
+      return arr if !arr.empty?
+      nil
     end 
 
-    def get_hash(args)
-      hash = {}
-      args.each do |arg|
-         hash_val = get_arg(arg)
+    def get_hash(args)    
+      return nil if !args.class == Ruby::Hash
+      items = args.respond_to?(:elements) ? args.elements : args 
+      hash = {}  
+      items.each do |arg|  
+         hash_val = get_arg(arg) 
          hash.merge!(hash_val)
-      end
-      hash
+      end    
+      return hash if !hash.empty?
+      nil
     end 
 
-
-    # TODO: Needs major refactoring!
-    def block_params?(value)
-      found = 0 
-      if respond_to? :block
-        p = self.block 
-        parameters = p.params.elements
-        parameters.each do |param| 
-          parameter = get_param(param.param) 
-          value.each do |v|                       
-            v = v[:array] if v.respond_to?(:has_key?) && v[:array]            
-            found += 1 if parameter == v
-          end
-        end  
-        return found == value.size
-      end
-      false
+    def get_assoc(arg)
+      return nil if !(arg.class == Ruby::Assoc) 
+      get_hash_item(arg)      
     end
 
-    # TODO: Needs major refactoring! 
-    def params?(value)
-      found = 0 
-      if respond_to? :params
-        parameters = params.elements
-        parameters.each do |param| 
-          parameter = get_param(param.param) 
-          value.each do |v|  
-            v = v[:array] if v.respond_to?(:has_key?) && v[:array]
-            found += 1 if parameter == v
-          end
-        end  
-        return found == value.size
-      end
-      false
-    end
-
-    def get_hash_item(arg)
-      key = get_identifier(arg.key) if key.respond_to? :identifier
-      key = get_identifier(arg.key).to_sym  if arg.key.class == Ruby::Symbol
-      key = get_token(arg.key) if arg.key.class == Ruby::Variable
-      value = get_token(arg.value)
-      value = value.to_i if arg.value.class == Ruby::Integer
-      value = value.to_sym if arg.value.class == Ruby::Symbol
-      value = value.to_f if arg.value.class == Ruby::Float
+    def get_hash_item(arg)   
+      return if !arg.respond_to? :key      
+      key = get_key(arg.key)
+      value = get_value(arg.value)
       return {key => value}
     end
 
+    def get_key(key)             
+      if key.respond_to? :identifier
+       id = get_identifier(key)
+      end
+      return id.to_sym if key.class == Ruby::Symbol
+      return get_token(key) if key.class == Ruby::Variable
+      id
+    end
+
+    # Needs rework!
+    def get_value(value)
+      real_value = get_arg(value)
+      convert_value(real_value, value)
+    end
+
     def get_identifier(arg)
-      get_token(arg.identifier)
+      get_token(arg.identifier) if arg.respond_to? :identifier
+    end
+
+    def get_string(arg)
+      get_token(arg.elements[0]) if arg.class == Ruby::String
     end
     
-    def get_token(arg)
-      return arg.elements[0].token if arg.class == Ruby::String        
-      arg.token
+    def get_token(arg)                             
+      arg.token if arg.respond_to? :token
     end
 
     def is_instance_of?(klass)
